342b042f0b06e5f2f88370a3ea97574453b193db
Vluuks <vluuks@gmail.com>
2017-06-22 09:57:47 +0200

    added firebase config

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,1 +1,33 @@
-var hoi = "yo";
+/* Created by Renske, for what it's worth. */
+
+/* Wait until page is ready. */
+$('document').ready(function() {
+
+    // Manage DOM element visibilities.
+    $('#error').hide();
+
+});
+
+
+/* On button click, process date and amount of foals. */
+function processFoals(){
+    
+    console.log("yo ho test");
+    
+}
+
+
+
+
+/* Insert into database. FIREBASE YO */
+
+
+
+
+
+/* Show visualization with D3. */
+/* Allow click on date with tooltip? */
+
+
+
+
